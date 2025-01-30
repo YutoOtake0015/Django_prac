65a89f210aced05038d2dd1b6e2c8f8a2a6d5cf9
YutoOtake <yuto.programming@gmail.com>
2025-01-30 22:32:57 +0900

    delete SampleModel. create Book Model

--- book/urls.py
+++ book/urls.py
@@ -1,5 +1,8 @@
 
-
+from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('book/', views.ListBookView.as_view()),
+    path("book_detail/<int:pk>", views.DetailBookView.as_view())
 ]
