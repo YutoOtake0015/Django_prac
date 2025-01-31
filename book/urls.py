2e5ade7f2faa4df5967abbc8344dbb159d9ee3a7
YutoOtake <yuto.programming@gmail.com>
2025-01-31 22:33:24 +0900

    add create form

--- book/urls.py
+++ book/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('book/', views.ListBookView.as_view()),
-    path("book_detail/<int:pk>", views.DetailBookView.as_view())
+    path('book/', views.ListBookView.as_view(), name='list-book'),
+    path("book_detail/<int:pk>", views.DetailBookView.as_view(), name='detail-book'),
+    path("book/create/", views.CreateBookView.as_view(), name='create-book'),
 ]
